886d506cad7aa8f75eb9bf88635d5cfa1129c53b
el mahjoub <mahjoubwel@gmail.com>
2021-05-26 15:17:45 +0200

    speech to text

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:private_nanny/Home.dart';
-import 'package:private_nanny/service.dart';
-import 'package:private_nanny/user.dart';
+import 'package:private_nanny/voice.dart';
 
 import 'login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(VoiceHome());
 }
 
 class MyApp extends StatelessWidget {
-  Service _userService = Service();
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      debugShowCheckedModeBanner: false, // deleted debug bar in AppBar Widget
-      title: 'private nanny',
-      theme: new ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
-      // home: StreamBuilder(
-      //   stream: _userService.user,
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.active) {
-      //       if (snapshot.hasData) {
-      //         return HomeScreen();
-      //       }
-
-      //       return LoginPage();
-      //     }
-
-      //     return SafeArea(
-      //       child: Scaffold(
-      //         body: Center(
-      //           child: Text('Loading...'),
-      //         ),
-      //       ),
-      //     );
-      //   },
-      // ),
-    );
+        debugShowCheckedModeBanner: false, // deleted debug bar in AppBar Widget
+        title: 'private nanny',
+        theme: new ThemeData(primarySwatch: Colors.blue),
+        home: LoginPage());
   }
 }
