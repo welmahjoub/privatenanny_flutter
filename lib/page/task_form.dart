c07f5c9e15d05674b6244667e38ae739e8792e38
Koitrin <koitrinstephanedesire@gmail.com>
2021-05-27 17:20:04 +0200

    task data added

--- lib/page/task_form.dart
+++ lib/page/task_form.dart
@@ -1,16 +1,25 @@
 
 import 'package:flutter/material.dart';
+import 'package:private_nanny/model/task.dart';
 import 'package:private_nanny/widget/datetime_piker.dart';
-
-
 
 class TaskFormPage extends StatefulWidget {
   @override
   State createState() => new _TaskFormPageState();
+  TaskFormPage({Key key, this.task}) : super(key: key);
+  Task task;
 }
 
 class _TaskFormPageState extends State<TaskFormPage> {
   bool _switchValue = false;
+  @override
+  void initState() {
+    super.initState();
+    _titleController.text = widget.task.title;
+    _detailController.text = widget.task.detail;
+  }
+  final TextEditingController _titleController = new TextEditingController();
+  final TextEditingController _detailController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,18 +27,20 @@ class _TaskFormPageState extends State<TaskFormPage> {
       appBar: AppBar(
         title: Text('Nouvelle tâche'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            _buildTextFields(),
-            Divider(
-              height: 40,
-            ),
-            _buildReminder()
-          ],
+      body: SingleChildScrollView(
+        child:  Container(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 16 ),
+          child: Column(
+            children: [
+              _buildTextFields(),
+              Divider(
+                height: 40,
+              ),
+              _buildReminder()
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 
@@ -46,12 +57,13 @@ class _TaskFormPageState extends State<TaskFormPage> {
                   });
                 }),
             Visibility(
-              child:  Container(
-                padding: EdgeInsets.only(left: 7),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: DatetimePickerWidget(),
+              child: Column(
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(left: 7),
+                      child: DatetimePickerWidget()
                   )
+                ],
               ),
               visible: _switchValue,
             )
@@ -67,15 +79,20 @@ class _TaskFormPageState extends State<TaskFormPage> {
           Container(
             child: TextFormField(
               maxLength: 30,
+              controller: _titleController,
               decoration: InputDecoration(
                 labelText: 'Intitulé',
               ),
+              onChanged: (String value) {
+                widget.task.title = value;
+              },
             ),
           ),
           Container(
             margin: EdgeInsets.only(top: 6),
             child: TextField(
               maxLength: 255,
+              controller: _detailController,
               minLines: 1,
               maxLines: 5,
               keyboardType: TextInputType.multiline,
