0e6fd6bb9857ed7f090a61340bc492d945f8663e
el mahjoub <mahjoubwel@gmail.com>
2021-06-02 16:07:09 +0200

    utility

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -118,7 +118,7 @@ class _HomeScreenState extends State<HomeScreen> {
       Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (context) => TaskFormPage(task: new Task(_text))));
+              builder: (context) => TaskFormPage(task: new Task())));
     }
   }
 }
