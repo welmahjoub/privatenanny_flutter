2ab295d618057f61be8f3236f5565dd8b0ccf6f5
el mahjoub <mahjoubwel@gmail.com>
2021-06-03 17:24:30 +0200

    redirect to home page if user is already connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:private_nanny/page/home.dart';
 import 'package:private_nanny/page/loginPage.dart';
+import 'package:private_nanny/service/auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,12 +11,23 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  AuthService auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        debugShowCheckedModeBanner: false, // deleted debug bar in AppBar Widget
-        title: 'private nanny',
-        theme: new ThemeData(primarySwatch: Colors.blue),
-        home: LoginPage());
+    if (auth.auth.currentUser != null) {
+      return MaterialApp(
+          debugShowCheckedModeBanner:
+              false, // deleted debug bar in AppBar Widget
+          title: 'private nanny',
+          theme: new ThemeData(primarySwatch: Colors.blue),
+          home: LoginPage());
+    } else {
+      return MaterialApp(
+          debugShowCheckedModeBanner:
+              false, // deleted debug bar in AppBar Widget
+          title: 'private nanny',
+          theme: new ThemeData(primarySwatch: Colors.blue),
+          home: HomeScreen());
+    }
   }
 }
